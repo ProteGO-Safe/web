638c676b1d5f49bbbc3f9972b68f0b1aa03e5023
jakubrekas <jakub.rekas@thecoders.io>
2020-03-20 22:01:42 +0100

    Remove interceptor;

--- src/repository/Repository.js
+++ src/repository/Repository.js
@@ -1,36 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
-const baseDomain = "https://api.safesafe.app";
+const baseDomain = 'https://api.safesafe.app';
 const baseURL = `${baseDomain}/covid19`;
 
 const Repository = axios.create({ baseURL });
 
-Repository.interceptors.request.use(config => {
-  config.headers = { "Model": "infermedica-pl" };
-  return config;
-}, error => Promise.reject(error));
-
-Repository.interceptors.response.use(
-    response => Promise.resolve(response),
-    ({ response }) => {
-        if (!response) {
-            // handle error
-        }
-
-        const {
-            data,
-            data: { error: message, errors },
-            status
-        } = response;
-
-        // handle error message
-
-        return Promise.reject({
-            errors,
-            message,
-            status
-        });
-    }
+Repository.interceptors.request.use(
+  config => {
+    config.headers = { Model: 'infermedica-pl' };
+    return config;
+  },
+  error => Promise.reject(error)
 );
 
 export default Repository;
