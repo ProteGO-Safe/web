123d14eb92be7357f6761b812d8f74e04d637028
jakubrekas <jakub.rekas@thecoders.io>
2020-03-25 08:24:30 +0100

    Fix registration summary;

--- src/views/Registration/components/Summary/Summary.js
+++ src/views/Registration/components/Summary/Summary.js
@@ -6,9 +6,9 @@ import {
   Brand,
   Button,
   Container,
-  FieldSet,
-  Imprint
+  FieldSet
 } from '../../../../components';
+import Imprint from '../../../../components/Imprint/Imprint';
 import AngleLeftIcon from '../../../../assets/img/icons/angle-left.svg';
 import UserImg from '../../../../assets/img/icons/user.svg';
 import * as constants from '../../../../constants';
@@ -18,21 +18,81 @@ const Summary = () => {
 
   const chronicSicks = (() => {
     const chronicSicks = [];
-    if (values[constants.FIELD_CHRONIC_SICK_1]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_1, description: values[constants.FIELD_CHRONIC_SICK_1_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_2]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_2, description: values[constants.FIELD_CHRONIC_SICK_2_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_3]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_3, description: values[constants.FIELD_CHRONIC_SICK_3_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_4]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_4, description: values[constants.FIELD_CHRONIC_SICK_4_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_5]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_5, description: values[constants.FIELD_CHRONIC_SICK_5_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_6]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_6, description: values[constants.FIELD_CHRONIC_SICK_6_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_7]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_7, description: values[constants.FIELD_CHRONIC_SICK_7_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_8]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_8, description: values[constants.FIELD_CHRONIC_SICK_8_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_9]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_9, description: values[constants.FIELD_CHRONIC_SICK_9_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_10]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_10, description: values[constants.FIELD_CHRONIC_SICK_10_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_11]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_11, description: values[constants.FIELD_CHRONIC_SICK_11_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_12]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_12, description: values[constants.FIELD_CHRONIC_SICK_12_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_13]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_13, description: values[constants.FIELD_CHRONIC_SICK_13_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_14]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_14, description: values[constants.FIELD_CHRONIC_SICK_14_DESC]});
-    if (values[constants.FIELD_CHRONIC_SICK_15]) chronicSicks.push({name: constants.FIELD_CHRONIC_SICK_15, description: values[constants.FIELD_CHRONIC_SICK_15_DESC]});
+    if (values[constants.FIELD_CHRONIC_SICK_1])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_1,
+        description: values[constants.FIELD_CHRONIC_SICK_1_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_2])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_2,
+        description: values[constants.FIELD_CHRONIC_SICK_2_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_3])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_3,
+        description: values[constants.FIELD_CHRONIC_SICK_3_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_4])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_4,
+        description: values[constants.FIELD_CHRONIC_SICK_4_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_5])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_5,
+        description: values[constants.FIELD_CHRONIC_SICK_5_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_6])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_6,
+        description: values[constants.FIELD_CHRONIC_SICK_6_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_7])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_7,
+        description: values[constants.FIELD_CHRONIC_SICK_7_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_8])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_8,
+        description: values[constants.FIELD_CHRONIC_SICK_8_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_9])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_9,
+        description: values[constants.FIELD_CHRONIC_SICK_9_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_10])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_10,
+        description: values[constants.FIELD_CHRONIC_SICK_10_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_11])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_11,
+        description: values[constants.FIELD_CHRONIC_SICK_11_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_12])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_12,
+        description: values[constants.FIELD_CHRONIC_SICK_12_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_13])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_13,
+        description: values[constants.FIELD_CHRONIC_SICK_13_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_14])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_14,
+        description: values[constants.FIELD_CHRONIC_SICK_14_DESC]
+      });
+    if (values[constants.FIELD_CHRONIC_SICK_15])
+      chronicSicks.push({
+        name: constants.FIELD_CHRONIC_SICK_15,
+        description: values[constants.FIELD_CHRONIC_SICK_15_DESC]
+      });
     return chronicSicks;
   })();
   return (
@@ -61,11 +121,13 @@ const Summary = () => {
           <h5 className="big">Podsumujmy:</h5>
         </div>
         <Imprint
-          smokeNumber={values[constants.FIELD_SMOKE_NUMBER]}
-          sex={values[constants.FIELD_SEX]}
-          bloodGroup={values[constants.FIELD_BLOOD_GROUP]}
-          age={values[constants.FIELD_AGE]}
-          chronicSicks={chronicSicks}
+          user={{
+            age: values[constants.FIELD_AGE],
+            bloodGroup: values[constants.FIELD_BLOOD_GROUP],
+            chronicSicks,
+            sex: values[constants.FIELD_SEX],
+            smokeNumber: values[constants.FIELD_SMOKE_NUMBER]
+          }}
         />
         <FieldSet>
           <Button onClick={handleSubmit} text="Zapisz metrykę" />
