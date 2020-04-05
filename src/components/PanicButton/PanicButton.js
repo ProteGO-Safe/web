887572a419d8066af1ee5890ce6948cd1d7ef397
Piotr Szlagura <piotr.szlagura@ageno.pl>
2020-04-05 22:53:47 +0200

    Using regular button component to render PanicButton

--- src/components/PanicButton/PanicButton.js
+++ src/components/PanicButton/PanicButton.js
@@ -3,29 +3,29 @@ import PropTypes from 'prop-types';
 
 import useModalContext from '../../hooks/useModalContext';
 import { Help } from '../../views/Help';
+import { Button } from '../Button';
 import './PanicButton.scss';
 
-const PanicButton = ({ disabled, height, icon, size, type }) => {
+const PanicButton = ({ disabled, height, size, type }) => {
   const { openModal } = useModalContext();
   const handleButtonClick = useCallback(() => {
     openModal(<Help />);
   }, [openModal]);
   return (
-    <button
+    <Button
       disabled={disabled}
-      className={`button button--${type} button--${height} button--size--${size}`}
+      type={type}
+      height={height}
+      size={size}
+      text="?"
       onClick={handleButtonClick}
-      type="button"
-    >
-      ?{icon && <img alt="icon" className="button__icon" src={icon} />}
-    </button>
+    />
   );
 };
 
 PanicButton.defaultProps = {
   disabled: false,
   height: 'small',
-  icon: '',
   size: 'small',
   type: 'secondary'
 };
@@ -33,7 +33,6 @@ PanicButton.defaultProps = {
 PanicButton.propTypes = {
   disabled: PropTypes.bool,
   height: PropTypes.oneOf(['small', 'normal']),
-  icon: PropTypes.string,
   size: PropTypes.oneOf(['small', 'medium', 'normal']),
   type: PropTypes.oneOf([
     'black',
