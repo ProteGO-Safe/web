74a54340d2cc43b9c866ab9ae2f4b5b2411dfa5a
mateusz.andruszko <mateusz.andruszko@thecoders.io>
2020-04-17 12:59:57 +0200

    328 - hidden btn back and bottom nav

--- src/views/Diagnosis/Diagnosis.js
+++ src/views/Diagnosis/Diagnosis.js
@@ -14,7 +14,7 @@ const Diagnosis = ({
   showNavigation
 }) => (
   <DiagnosisWrapper className="view view__diagnosis view__diagnosis--single">
-    <Header onBackClick={clearDiagnosis} hideMenuButton />
+    <Header onBackClick={clearDiagnosis} hideMenuButton hideBackButton />
     <Container>
       {!isLoading && inProgress && question && question.type === 'single' && (
         <Single question={question} />
