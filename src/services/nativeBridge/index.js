8601bbd59ed049d85d9fa2213890d7cb9a82d6da
Mariusz Wójcik <pixiproject@gmail.com>
2020-04-22 20:01:35 +0200

    Set new way to communicate from native to web

--- src/services/nativeBridge/index.js
+++ src/services/nativeBridge/index.js
@@ -1,5 +1,9 @@
 import invoke from 'lodash.invoke';
 import uniqueId from 'lodash.uniqueid';
+import { cond, equals, always } from 'ramda';
+
+import StoreRegistry from '../../store/storeRegistry';
+import { NATIVE_DATA_SET_SERVICES_STATUS_SUCCESS } from '../../store/types/nativeData';
 import { DATA_TYPE } from './nativeBridge.constants';
 
 const isAndroidWebView = () => !!window.NativeBridge;
@@ -27,8 +31,6 @@ const receiveNativeResponse = (body, dataType, requestId) => {
   }
   delete nativeRequests[requestId];
 };
-
-window.bridgeDataResponse = receiveNativeResponse;
 
 const callNativeFunction = async (functionName, dataType, data) => {
   const args = [dataType];
@@ -65,6 +67,38 @@ const setDiagnosisTimestamp = async timestamp => {
     timestamp
   });
 };
+
+const handleServicesStatus = data => {
+  const store = StoreRegistry.getStore();
+  store.dispatch({
+    servicesStatus: data,
+    type: NATIVE_DATA_SET_SERVICES_STATUS_SUCCESS
+  });
+};
+
+const callBridgeDataHandler = cond([
+  [equals(DATA_TYPE.APP_STATE), always(handleServicesStatus)],
+  [
+    equals(DATA_TYPE.BATTERY_PERFORMANCE_PERMISSION),
+    always(handleServicesStatus)
+  ],
+  [equals(DATA_TYPE.BT_PERMISSION), always(handleServicesStatus)],
+  [equals(DATA_TYPE.LOCATION_PERMISSION), always(handleServicesStatus)],
+  [equals(DATA_TYPE.NOTIFICATION_PERMISSION), always(handleServicesStatus)],
+  [equals(DATA_TYPE.OPEN_TRACE), always(handleServicesStatus)]
+]);
+
+const onBridgeData = (dataType, dataString) => {
+  try {
+    const data = JSON.parse(dataString);
+    callBridgeDataHandler(dataType)(data);
+  } catch (error) {
+    console.error('Error while parsing native request', error);
+  }
+};
+
+window.onBridgeData = onBridgeData;
+window.bridgeDataResponse = receiveNativeResponse;
 
 export default {
   setDiagnosisTimestamp,
