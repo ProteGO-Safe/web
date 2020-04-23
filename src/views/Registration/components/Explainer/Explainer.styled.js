271dcaaf02ef85c6845140e71b5afdbd747816d0
mateusz.andruszko <mateusz.andruszko@thecoders.io>
2020-04-23 13:59:43 +0200

    PSAFE-363 - color/weight fonts

--- src/views/Registration/components/Explainer/Explainer.styled.js
+++ src/views/Registration/components/Explainer/Explainer.styled.js
@@ -7,10 +7,14 @@ export const Content = styled.div`
 
 export const SliderWrapper = styled.div`
   width: 100%;
-  margin-bottom: 50px;
 `;
 
-export const Title = styled.h3`
+export const Title = styled.h1`
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 1.3;
   text-align: center;
+  color: #1b1b1b;
+  margin-top: 0;
   margin-bottom: 0;
 `;
