4ed753335452d54b4b111d0fb36c7eb246d26f60
tmolenda <tomasz.molenda@thecoders.io>
2020-04-28 15:51:21 +0200

    PSAFE-555 - clear data bt in native

--- src/views/UserDataSettings/components/ModalConfirmation/ModalConfirmation.js
+++ src/views/UserDataSettings/components/ModalConfirmation/ModalConfirmation.js
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 import { Button } from '../../../../components';
 import useModalContext from '../../../../hooks/useModalContext';
-
 import Routes from '../../../../routes';
-import { useHistory } from 'react-router-dom';
 import useLoaderContext from '../../../../hooks/useLoaderContext';
+import { clearBluetoothData } from '../../../../store/actions/nativeData';
 
 const ModalConfirmation = () => {
+  const dispatch = useDispatch();
   const { onClose } = useModalContext();
   const history = useHistory();
   const { setLoader } = useLoaderContext();
 
   const handleClose = () => {
+    dispatch(clearBluetoothData());
     localStorage.clear();
 
     onClose();
