d4ac8776d19a51028caf3089cc6583988a860665
tmolenda <tomasz.molenda@thecoders.io>
2020-05-12 11:03:30 +0200

    PSAFE-743 - change badge to show in third day

--- src/utills/badge/index.js
+++ src/utills/badge/index.js
@@ -9,14 +9,27 @@ const containsDayAgo = (daysAmount, today, days) => {
   );
 };
 
+const containsThisDay = (today, days) => {
+  return (
+    days.filter(timestamp => {
+      const day = moment.unix(timestamp);
+      return day.isSame(today, 'day');
+    }).length > 0
+  );
+};
+
 // filledDays contains timestamps
 const hasBadge = (filledDays = []) => {
   const today = moment();
 
+  if (filledDays.length < 3) {
+    return false;
+  }
+
   return (
+    containsThisDay(today, filledDays) &&
     containsDayAgo(1, today, filledDays) &&
-    containsDayAgo(2, today, filledDays) &&
-    containsDayAgo(3, today, filledDays)
+    containsDayAgo(2, today, filledDays)
   );
 };
 
