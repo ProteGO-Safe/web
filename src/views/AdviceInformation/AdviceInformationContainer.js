4a1347f9d84b2eb4d0140bd4dafba1224076260c
mateusz.andruszko <mateusz.andruszko@thecoders.io>
2020-05-20 07:02:20 +0000

    PSAFE-952 - Add advices

---
+++ src/views/AdviceInformation/AdviceInformationContainer.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Container, Content, View } from '../../theme/grid';
+import { Header, BottomNavigation } from '../../components';
+import AdviceInformation from './AdviceInformation';
+
+import IconAdviceHome from '../../assets/img/icons/zostan-w-domu.svg';
+import IconAdvicePhone from '../../assets/img/icons/seniorzy.svg';
+import IconAdviceCountry from '../../assets/img/icons/przyjazd.svg';
+import IconAdviceNote from '../../assets/img/icons/dziennik.svg';
+
+import AdviceList from './advice.json';
+
+const AdviceInformationContainer = () => {
+  const { advices } = AdviceList;
+  const { watermark } = AdviceList;
+  const tipsStatic = [
+    {
+      icon: IconAdviceHome,
+      text: 'Zostań w domu.'
+    },
+    {
+      icon: IconAdvicePhone,
+      text:
+        'Zadzwoń do rodziców i krewnych w podeszłym wieku. Pomóż im korzystać z ProteGo Safe na ich telefonie. Zrób dla nich zakupy. Unikaj kontaktu osobistego.'
+    },
+    {
+      icon: IconAdviceCountry,
+      text:
+        'Jeśli przyjechałeś/-aś z zagranicy – skontaktuj się z sanepidem i poddaj obowiązkowej 14-dniowej kwarantannie.'
+    },
+    {
+      icon: IconAdviceNote,
+      text:
+        'Regularnie uzupełniaj zakładkę [url]MÓJ DZIENNIK ZDROWIA|/daily[url]: zapisuj w aplikacji objawy i temperaturę ciała.'
+    }
+  ];
+  return (
+    <View>
+      <Header hideBackButton />
+      <Content>
+        <Container className="full-height tips">
+          <AdviceInformation
+            title="Porady"
+            tips={tipsStatic}
+            collapse={advices}
+            watermark={watermark}
+          />
+        </Container>
+        <BottomNavigation />
+      </Content>
+    </View>
+  );
+};
+
+export default AdviceInformationContainer;
--- src/views/AdviceInformation/index.js
+++ src/views/AdviceInformation/index.js
@@ -1,1 +1,1 @@
-export { default as AdviceInformation } from './AdviceInformation';
+export { default as AdviceInformation } from './AdviceInformationContainer';
