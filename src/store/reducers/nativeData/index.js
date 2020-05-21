8424b1bc1909cefc5bf45fb2c6b22792697c7bc0
tmolenda <tomasz.molenda@thecoders.io>
2020-05-21 19:26:17 +0200

    PSAFE-959 - handle exposure status

--- src/store/reducers/nativeData/index.js
+++ src/store/reducers/nativeData/index.js
@@ -3,10 +3,12 @@ import {
   NATIVE_DATA_FETCH_NOTIFICATION_SUCCESS,
   NATIVE_DATA_FETCH_SERVICES_STATUS_SUCCESS,
   NATIVE_DATA_HIDE_NOTIFICATION_SUCCESS,
-  NATIVE_DATA_SET_SERVICES_STATUS_SUCCESS
+  NATIVE_DATA_SET_SERVICES_STATUS_SUCCESS,
+  EXPOSURE_SUMMARY_FETCHED
 } from '../../types/nativeData';
 
 const INITIAL_STATE = {
+  exposureSummary: undefined,
   notification: undefined,
   servicesStatus: {},
   servicesStatusSetByNative: false
@@ -54,6 +56,13 @@ const nativeBridgeReducer = (state = INITIAL_STATE, action) => {
         ...state,
         servicesStatusSetByNative: false
       };
+    case EXPOSURE_SUMMARY_FETCHED: {
+      const { exposureSummary } = action;
+      return {
+        ...state,
+        exposureSummary
+      };
+    }
     default:
       return state;
   }
