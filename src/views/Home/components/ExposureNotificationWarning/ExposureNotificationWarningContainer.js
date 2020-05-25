009fd9e3023838ae3170866dc5bbb661f1c1ef25
marta.palczynska <marta.palczynska@thecoders.io>
2020-05-25 21:19:04 +0000

    PSAFE-1068 tracing notification

--- src/views/Home/components/ExposureNotificationWarning/ExposureNotificationWarningContainer.js
+++ src/views/Home/components/ExposureNotificationWarning/ExposureNotificationWarningContainer.js
@@ -4,6 +4,9 @@ import { isIOSWebView } from '../../../../utils/native';
 import { enableServices } from '../../../../store/actions/nativeData';
 import { ExposureNotificationWarning } from './ExposureNotificationWarning';
 import { EXPOSURE_NOTIFICATION_STATUS } from '../../../../utils/servicesStatus/servicesStatus.constants';
+import { ExposureNotificationInfo } from './ExposureNotificationInfo';
+import WifiIcon from '../../../../assets/img/icons/wifi.gif';
+import { FakeIcon } from './FakeIcon';
 
 const ExposureNotificationWarningContainer = () => {
   const {
@@ -14,11 +17,19 @@ const ExposureNotificationWarningContainer = () => {
       exposureNotificationStatus
     }
   } = useSelector(state => state.nativeData);
+
   const showServiceWarning =
     (!isIOSWebView() && isLocationOn === false) ||
     isBtOn === false ||
     isNotificationEnabled === false ||
     exposureNotificationStatus === EXPOSURE_NOTIFICATION_STATUS.OFF;
+
+  const showEnableServicesInfo =
+    !isIOSWebView() &&
+    isLocationOn &&
+    isBtOn &&
+    isNotificationEnabled &&
+    exposureNotificationStatus === EXPOSURE_NOTIFICATION_STATUS.ON;
 
   const handleEnableServices = () => {
     const servicesState = {};
@@ -37,10 +48,17 @@ const ExposureNotificationWarningContainer = () => {
     enableServices(servicesState);
   };
 
+  if (showEnableServicesInfo) {
+    return <ExposureNotificationInfo icon={WifiIcon} />;
+  }
+
   return (
-    showServiceWarning && (
-      <ExposureNotificationWarning enableServices={handleEnableServices} />
-    )
+    <>
+      {showServiceWarning && (
+        <ExposureNotificationWarning enableServices={handleEnableServices} />
+      )}
+      <FakeIcon icon={WifiIcon} />
+    </>
   );
 };
 
