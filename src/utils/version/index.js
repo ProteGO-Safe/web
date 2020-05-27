172fb4164aca3dc4162df24ccab3adeed29a7792
tmolenda <tomasz.molenda@thecoders.io>
2020-05-27 16:47:21 +0200

    PSAFE-1098 - disable settnigs in not suported

--- src/utils/version/index.js
+++ src/utils/version/index.js
@@ -1,7 +1,16 @@
+import { EXPOSURE_NOTIFICATION_STATUS } from '../servicesStatus/servicesStatus.constants';
+
 export const isVersionCompatibilityWithBluetoothModule = (
   servicesStatus = {}
 ) => {
   if (Object.keys(servicesStatus).length === 0) {
+    return false;
+  }
+  const { exposureNotificationStatus } = servicesStatus;
+
+  if (
+    exposureNotificationStatus === EXPOSURE_NOTIFICATION_STATUS.NOT_SUPPORTED
+  ) {
     return false;
   }
   return true;
