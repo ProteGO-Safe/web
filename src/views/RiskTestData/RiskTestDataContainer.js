d7d37dc1461236f4bcf33ad0cc0824a34e192bec
koszatnik12 <palczynskamarta@gmail.com>
2020-05-30 17:31:55 +0200

    PSAFE-1158 countries in rist test data (#73)
    * PSAFE-1158 countries in rist test data
    
    * PSAFE-1158 remove logic from RistTestData
    
    Co-authored-by: Marta Pałczyńska <marta.palczynska@avra.pl>

--- src/views/RiskTestData/RiskTestDataContainer.js
+++ src/views/RiskTestData/RiskTestDataContainer.js
@@ -5,6 +5,7 @@ import moment from 'moment';
 
 import RiskTestData from './RiskTestData';
 import { TRIAGE_LEVEL } from './RiskTestData.constants';
+import locations from '../../services/diagnosisLogic/locations.json';
 
 const dateFormat = 'D-MM-YYYY';
 
@@ -16,10 +17,27 @@ const RiskTestDataContainer = () => {
 
   const { allQuestions, evidence, triageLevel } = riskTest[[id]];
 
+  const getCountries = () => {
+    const countryAnswers = evidence.filter(_obj => _obj.id.startsWith('l'));
+    const countryNames = countryAnswers.map(
+      answer => locations.find(country => country.id === answer.id).name
+    );
+    return countryNames.map((name, index) => (
+      <>
+        {name}
+        {index !== countryNames.length - 1 ? ', ' : null}
+      </>
+    ));
+  };
+
   const idToChoiceResolver = (evidenceId, choices) => {
+    if (evidenceId === 'p_5') {
+      return getCountries();
+    }
     const answer = evidence.find(_obj => _obj.id === evidenceId);
     const choiceId = answer ? answer.choice_id : 'absent';
-    return choices.find(_obj => _obj.id === choiceId).label;
+    const choice = choices.find(_obj => _obj.id === choiceId);
+    return choice ? choice.label : '';
   };
 
   const { riskTestInformation } = TRIAGE_LEVEL[triageLevel];
