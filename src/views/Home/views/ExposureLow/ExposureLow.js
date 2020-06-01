ffe03b12e308f70b660d112cbe1de5e48255a329
TomaszMolenda <tomasz.molenda@thecoders.io>
2020-06-01 08:44:18 +0200

    PSAFE-1179 - hide start diagnosis button (#76)

--- src/views/Home/views/ExposureLow/ExposureLow.js
+++ src/views/Home/views/ExposureLow/ExposureLow.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   Smile,
-  MakeDiagnosisButton,
   TriageInfoBorder
 } from '../../components/index';
 import { Paragraph } from '../../../../theme/typography';
@@ -15,7 +14,6 @@ const ExposureLow = () => {
         Jeśli chcesz dowiedzieć się więcej na temat czynników mających wpływ na
         ryzyko infekcji, kliknij poniżej i wykonaj test oceny ryzyka.
       </Paragraph>
-      <MakeDiagnosisButton />
     </>
   );
 };
