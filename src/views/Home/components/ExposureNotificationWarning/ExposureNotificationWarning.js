08fd5332cdb95baf92032ad442eb60519ec61a3c
Mateusz Andruszko <64523688+mateuszandruszko@users.noreply.github.com>
2020-06-02 08:25:15 +0200

    PSAFE-1189 - change information block on the dashboard (#91)

--- src/views/Home/components/ExposureNotificationWarning/ExposureNotificationWarning.js
+++ src/views/Home/components/ExposureNotificationWarning/ExposureNotificationWarning.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Warning } from '../../../../components';
+import { Color } from '../../../../theme/colors';
 
 const ExposureNotificationWarning = ({ enableServices }) => {
   return (
     <Warning
+      arrow
       onClick={enableServices}
+      colorFont={Color.secondary}
       status="error"
-      title="Powiadamianie o możliwym kontakcie z koronawirusem jest nieaktywne"
-      content="Kliknij, aby je włączyć."
+      title="Powiadamianie o możliwym kontakcie z koronawirusem jest wyłączone"
+      content="Kliknij, aby włączyć monitorowanie ryzyka"
     />
   );
 };
