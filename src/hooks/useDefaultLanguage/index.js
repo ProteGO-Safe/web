96707ed67a7cd138763bd47d36ad018f824bb612
TomaszMolenda <tomasz.molenda@thecoders.io>
2020-08-20 09:33:46 +0200

    PSAFE-1925 - disable change languages (#248)

--- src/hooks/useDefaultLanguage/index.js
+++ src/hooks/useDefaultLanguage/index.js
@@ -1,20 +1,16 @@
-import { useSelector } from 'react-redux';
-import { getNativeLanguage } from '../../store/selectors/nativeData';
 import { DEFAULT_LANGUAGE } from '../../constants';
-import { languages } from '../../utils/languages';
-import { getAppLanguage } from '../../store/selectors/app';
 
 const useDefaultLanguage = () => {
-  const nativeLanguage = useSelector(getNativeLanguage);
-  const appLanguage = useSelector(getAppLanguage);
+  // const nativeLanguage = useSelector(getNativeLanguage);
+  // const appLanguage = useSelector(getAppLanguage);
 
   const defaultLanguage = (() => {
-    if (appLanguage) {
-      return appLanguage;
-    }
-    if (nativeLanguage && languages.includes(nativeLanguage)) {
-      return nativeLanguage;
-    }
+    // if (appLanguage) {
+    //   return appLanguage;
+    // }
+    // if (nativeLanguage && languages.includes(nativeLanguage)) {
+    //   return nativeLanguage;
+    // }
     return DEFAULT_LANGUAGE;
   })();
 
