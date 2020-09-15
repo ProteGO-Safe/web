41a3e3b6b3c5a8519386cadeeb3444221df25160
TomaszMolenda <tomasz.molenda@thecoders.io>
2020-09-15 08:55:17 +0200

    PSAFE-2138 - fix id in radio button (#301)

--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import uniqueId from 'lodash.uniqueid';
 import { Error } from '../index';
 import * as Styled from './Radio.styled';
 
-const Radio = ({ checked, content, error, label, name, onChange }) => (
-  <Styled.Wrapper>
-    <Styled.Radio
-      id={label}
-      checked={checked}
-      name={name}
-      onChange={onChange}
-      type="radio"
-    />
-    <Styled.Label htmlFor={label} error={error} hasContent={!!content}>
-      <Styled.Text>{label}</Styled.Text>
-      {content && content}
-    </Styled.Label>
-    {error && <Error content={error} />}
-  </Styled.Wrapper>
-);
+const Radio = ({ checked, content, error, label, name, onChange }) => {
+  const id = uniqueId('radio-');
+  return (
+    <Styled.Wrapper>
+      <Styled.Radio
+        id={id}
+        checked={checked}
+        name={name}
+        onChange={onChange}
+        type="radio"
+      />
+      <Styled.Label htmlFor={id} error={error} hasContent={!!content}>
+        <Styled.Text>{label}</Styled.Text>
+        {content && content}
+      </Styled.Label>
+      {error && <Error content={error} />}
+    </Styled.Wrapper>
+  );
+};
 
 Radio.defaultProps = {
   checked: false,
