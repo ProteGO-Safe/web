57e4de4009eb0fa85ba2114715d406208b13dadc
TomaszMolenda <tomasz.molenda@thecoders.io>
2020-10-27 18:51:20 +0100

    PSAFE-XXXX - fixed tor result (#454)

--- src/views/SummaryRiskTest/SummaryRiskTestContainer.js
+++ src/views/SummaryRiskTest/SummaryRiskTestContainer.js
@@ -3,31 +3,33 @@ import { Layout } from '../../components';
 import { DATA } from './SummaryRiskTest.helpers';
 import SummaryRiskTest from './SummaryRiskTest';
 import useTriage from '../../hooks/useTriage';
+import useLabTest from '../../hooks/useLabTest';
 
 const SummaryRiskTestContainer = () => {
-  const { isExposure, triageRiskLevel } = useTriage();
+  const { triageRiskLevel } = useTriage();
+  const { isEnHigh } = useLabTest();
 
   const screenData = useMemo(() => {
-    if (triageRiskLevel === 1 && !isExposure) {
+    if (triageRiskLevel === 1 && !isEnHigh) {
       return DATA.TOR_GREEN_EN_NOT_RED;
     }
-    if (triageRiskLevel === 2 && !isExposure) {
+    if (triageRiskLevel === 2 && !isEnHigh) {
       return DATA.TOR_ORANGE_EN_NOT_RED;
     }
-    if (triageRiskLevel === 3 && !isExposure) {
+    if (triageRiskLevel === 3 && !isEnHigh) {
       return DATA.TOR_RED_EN_NOT_RED;
     }
-    if (triageRiskLevel === 1 && isExposure) {
+    if (triageRiskLevel === 1 && isEnHigh) {
       return DATA.TOR_GREEN_EN_RED;
     }
-    if (triageRiskLevel === 2 && isExposure) {
+    if (triageRiskLevel === 2 && isEnHigh) {
       return DATA.TOR_ORANGE_EN_RED;
     }
-    if (triageRiskLevel === 3 && isExposure) {
+    if (triageRiskLevel === 3 && isEnHigh) {
       return DATA.TOR_RED_EN_RED;
     }
     return DATA.TOR_RED_EN_RED;
-  }, [triageRiskLevel, isExposure]);
+  }, [triageRiskLevel, isEnHigh]);
 
   return (
     <Layout hideBackButton noPadding fullHeight>
