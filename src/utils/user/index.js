c9ffd1065234c997b1043f20b79f92856ab8edb4
pawelmajthecoders <72006552+pawelmajthecoders@users.noreply.github.com>
2020-10-27 12:32:47 +0100

    PSAFE-2647 make user nick nullable (#446)
    Signed-off-by: Paweł Maj <majpawel90@gmail.com>
    
    Co-authored-by: Paweł Maj <majpawel90@gmail.com>

--- src/utils/user/index.js
+++ src/utils/user/index.js
@@ -3,4 +3,5 @@ import * as Yup from 'yup';
 export const userNameValidationSchema = Yup.string()
   .min(1, 'name_form_text7')
   .max(12, 'name_form_text8')
-  .matches(/[^\s\\]/, 'name_form_text9');
+  .matches(/[^\s\\]/, 'name_form_text9')
+  .nullable();
