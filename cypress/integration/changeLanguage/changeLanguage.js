fbc44b0998d0244451ad9cacb6c03a67a582f7b0
Jakub Rękas <jakubrekas1@gmail.com>
2020-10-30 22:35:02 +0100

    SCPSU-387: Cypress configuration; e2e change language;

---
+++ cypress/integration/changeLanguage/changeLanguage.js
@@ -0,0 +1,41 @@
+import { visitBaseUrl } from '../../utils/visitBaseUrl';
+import { resolveChangeLanguage } from './changeLanguage.helpers';
+
+/**
+ * Change language
+ */
+describe('Choose language on first screen', () => {
+  visitBaseUrl();
+
+  const changeToPolish = resolveChangeLanguage('#select_flag_PL', 'PL');
+  const changeToEnglish = resolveChangeLanguage('#select_flag_GB', 'GB');
+  const changeToUkrainian = resolveChangeLanguage('#select_flag_UA', 'UA');
+
+  it('should change language to polish', () => {
+    changeToPolish();
+  });
+
+  it('should change language to english', () => {
+    changeToEnglish();
+  });
+
+  it('should change language to ukrainian', () => {
+    changeToUkrainian();
+  });
+
+  it('should change language to english', () => {
+    changeToEnglish();
+  });
+
+  it('should change language to polish', () => {
+    changeToPolish();
+  });
+
+  it('should change language to ukrainian', () => {
+    changeToEnglish();
+  });
+
+  it('should change language to polish', () => {
+    changeToPolish();
+  });
+});
