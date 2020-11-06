7e86be7ad0e1543f42ab3b2f41a60d02b5f002fd
TomaszMolenda <tomasz.molenda@thecoders.io>
2020-11-06 19:54:35 +0100

    PSAFE-2929 - changed revoke covid from 14 to 10 days (#501)

--- src/views/Home/HomeContainer.js
+++ src/views/Home/HomeContainer.js
@@ -6,21 +6,29 @@ import {
   fetchExposureNotificationStatistics,
   fetchServicesStatus
 } from '../../store/actions/nativeData';
-import { resetTimeOfConfirmedCovid } from '../../store/actions/triage';
+import {
+  resetTimeOfConfirmedCovid,
+  revokeManualCovid
+} from '../../store/actions/triage';
 import { getSubscribedDistricts } from '../../store/selectors/restrictions';
+import { getTimeOfConfirmedManualCovid } from '../../store/selectors/triage';
 
 const HomeContainer = () => {
   const dispatch = useDispatch();
   const { timeOfConfirmedCovid } = useSelector(state => state.triage);
+  const timeOfConfirmedManualCovid = useSelector(getTimeOfConfirmedManualCovid);
   const subscribedDistricts = useSelector(getSubscribedDistricts);
 
   useEffect(() => {
     dispatch(fetchServicesStatus());
     dispatch(fetchExposureNotificationStatistics());
-    if (moment().diff(moment.unix(timeOfConfirmedCovid), 'days') >= 14) {
+    if (moment().diff(moment.unix(timeOfConfirmedCovid), 'days') >= 10) {
       dispatch(resetTimeOfConfirmedCovid());
     }
-  }, [dispatch, timeOfConfirmedCovid]);
+    if (moment().diff(moment.unix(timeOfConfirmedManualCovid), 'days') >= 10) {
+      dispatch(revokeManualCovid());
+    }
+  }, [dispatch, timeOfConfirmedCovid, timeOfConfirmedManualCovid]);
 
   return <Home subscribedDistricts={subscribedDistricts} />;
 };
