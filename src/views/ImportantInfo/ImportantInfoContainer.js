e64f2a9def2c8c10668761524c65740c7f2fc38a
Mateusz Andruszko <64523688+mateuszandruszko@users.noreply.github.com>
2020-11-12 19:31:23 +0100

    PSAFE-2959 (#512)

--- src/views/ImportantInfo/ImportantInfoContainer.js
+++ src/views/ImportantInfo/ImportantInfoContainer.js
@@ -39,8 +39,7 @@ const ImportantInfoContainer = ({ t }) => {
       icon: <Icon3 />,
       title: t('important_info_title_3'),
       description: t('important_info_description_3'),
-      path: Routes.CurrentRestrictions,
-      newFeature: true
+      path: Routes.CurrentRestrictions
     },
     {
       type: 'link',
