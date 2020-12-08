2531c292012a562173c32a229b64504e4a5303e8
Mateusz Andruszko <mateusz.andruszko@thecoders.io>
2020-12-08 19:04:14 +0100

    PSAFE-3218 - del import component

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Layout } from '../../components';
-import { Dashboard, FollowDistrictsSlider, InfoInteroperability, RatingApp } from '../index';
+import { FollowDistrictsSlider, InfoInteroperability, RatingApp } from '../index';
 import { ExposureNotificationWarning, HomeContent } from './components';
 import * as Styled from './Home.styled';
 
 const Home = ({ subscribedDistricts }) => {
   return (
     <Layout id="view-home" hideBackButton isNavigation noMargin>
-      <Dashboard />
-
       <Styled.Container>
         <ExposureNotificationWarning />
       </Styled.Container>
