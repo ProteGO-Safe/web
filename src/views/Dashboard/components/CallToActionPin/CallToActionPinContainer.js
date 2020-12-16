dba123575478126242aeaf26ba3b5a7678b2e28b
TomaszMolenda <tomasz.molenda@thecoders.io>
2020-12-16 23:20:16 +0100

    PSAFE-3290 - enable native services cta pin (#650)

--- src/views/Dashboard/components/CallToActionPin/CallToActionPinContainer.js
+++ src/views/Dashboard/components/CallToActionPin/CallToActionPinContainer.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { not, ifElse } from 'ramda';
 import CallToActionPin from './CallToActionPin';
 import useNavigation from '../../../../hooks/useNavigation';
 import { Routes } from '../../../../services/navigationService/routes';
 import useHealthStats from '../../../../hooks/useHealthStats';
+import useSupportExposureNotificationTracing from '../../../../hooks/useSupportExposureNotificationTracing';
 
 const CallToActionPinContainer = () => {
   const { goTo } = useNavigation();
   const { isCovidConfirmed } = useHealthStats();
+  const { areEnableAllServices, handleEnableServices } = useSupportExposureNotificationTracing();
+  const [pressed, setPressed] = useState(false);
+
+  const handlePressed = ifElse(
+    () => areEnableAllServices,
+    () => goTo(Routes.UploadHistoricalData),
+    handleEnableServices
+  );
+
+  useEffect(() => {
+    if (not(pressed)) {
+      return;
+    }
+    handlePressed();
+  }, [pressed, handlePressed]);
 
   if (isCovidConfirmed) {
     return null;
   }
 
-  return <CallToActionPin onClick={() => goTo(Routes.UploadHistoricalData)} />;
+  return <CallToActionPin onClick={() => setPressed(true)} />;
 };
 
 export default CallToActionPinContainer;
