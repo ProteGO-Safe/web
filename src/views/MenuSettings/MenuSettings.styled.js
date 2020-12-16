5c988ecf6f15d389e95943b6053e79bf4d68bb48
Mateusz Andruszko <64523688+mateuszandruszko@users.noreply.github.com>
2020-12-16 21:30:15 +0100

    PSAFE-3287 (#648)

--- src/views/MenuSettings/MenuSettings.styled.js
+++ src/views/MenuSettings/MenuSettings.styled.js
@@ -12,14 +12,14 @@ export const Item = styled.span`
   flex-flow: wrap row;
   align-items: center;
   width: 100%;
-  height: 64px;
+  min-height: 60px;
   padding: 0 24px 0 30px;
   color: ${Color.primary};
   border-bottom: 1px solid ${Color.gray_1};
   pointer-events: ${({ disabled }) => disabled && 'none'};
   cursor: ${({ disabled }) => disabled && 'default'};
   opacity: ${({ disabled }) => (disabled ? '0.4' : '1')};
-  @media (max-width: 300px) {
+  @media (max-width: 320px) {
     padding: 0 17px 0 24px;
   }
 `;
@@ -31,7 +31,7 @@ export const Arrow = styled(ArrowRight)`
   width: 14px;
   height: 14px;
   margin-top: -7px;
-  @media (max-width: 300px) {
+  @media (max-width: 320px) {
     right: 17px;
   }
 `;
@@ -44,10 +44,10 @@ export const Name = styled.span`
   line-height: 24px;
   font-weight: 700;
   color: ${Color.primary};
-  @media (max-width: 300px) {
+  @media (max-width: 320px) {
     width: calc(100% - 54px);
     margin-left: 54px;
-    font-size: 14px;
+    font-size: 16px;
     line-height: 20px;
   }
 `;
@@ -67,7 +67,7 @@ export const IconWrapper = styled.div`
     max-width: 100%;
     max-height: 100%;
   }
-  @media (max-width: 300px) {
+  @media (max-width: 320px) {
     left: 24px;
     width: 30px;
   }
