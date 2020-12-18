9b55636e21498d9770379603f7ee6b8a426e9e56
Mateusz Andruszko <64523688+mateuszandruszko@users.noreply.github.com>
2020-12-18 13:20:27 +0100

    PSAFE-3325 (#671)

--- src/views/Information/Information.js
+++ src/views/Information/Information.js
@@ -1,54 +1,41 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Button, FieldSet, Layout, T } from '../../components';
-import {
-  Title,
-  Paragraph1,
-  Paragraph2,
-  WarningContent,
-  WarningLabel,
-  WarningList,
-  WarningListItem
-} from './Information.styled';
+import * as Styled from './Information.styled';
 
 import warning from '../../assets/img/icons/warning.svg';
 
-const Information = ({ hideInformation }) => {
-  const { triageLevel } = useSelector(state => state.triage);
-
-  return (
-    <Layout id="view-information" hideBackButton={triageLevel === ''} hideBell>
-      <Title>
-        <T i18nKey="information_text1" />
-      </Title>
-      <Paragraph1>
-        <T i18nKey="information_text2" />
-      </Paragraph1>
-      <WarningContent>
-        <WarningLabel>
-          <img src={warning} alt={<T i18nKey="information_text3" />} />
-          <T i18nKey="information_text3" />
-        </WarningLabel>
-        <Paragraph2>
-          <T i18nKey="information_text4" />
-        </Paragraph2>
-        <WarningList>
-          <WarningListItem>
-            <T i18nKey="information_text5" />
-          </WarningListItem>
-          <WarningListItem>
-            <T i18nKey="information_text7" />
-          </WarningListItem>
-          <WarningListItem>
-            <T i18nKey="information_text10" />
-          </WarningListItem>
-        </WarningList>
-      </WarningContent>
-      <FieldSet>
-        <Button onClick={hideInformation} label={<T i18nKey="button_next" />} />
-      </FieldSet>
-    </Layout>
-  );
-};
+const Information = ({ hideInformation }) => (
+  <Layout id="view-information" hideBell>
+    <Styled.Title>
+      <T i18nKey="information_text1" />
+    </Styled.Title>
+    <Styled.Paragraph1>
+      <T i18nKey="information_text2" />
+    </Styled.Paragraph1>
+    <Styled.WarningContent>
+      <Styled.WarningLabel>
+        <img src={warning} alt={<T i18nKey="information_text3" />} />
+        <T i18nKey="information_text3" />
+      </Styled.WarningLabel>
+      <Styled.Paragraph2>
+        <T i18nKey="information_text4" />
+      </Styled.Paragraph2>
+      <Styled.WarningList>
+        <Styled.WarningListItem>
+          <T i18nKey="information_text5" />
+        </Styled.WarningListItem>
+        <Styled.WarningListItem>
+          <T i18nKey="information_text7" />
+        </Styled.WarningListItem>
+        <Styled.WarningListItem>
+          <T i18nKey="information_text10" />
+        </Styled.WarningListItem>
+      </Styled.WarningList>
+    </Styled.WarningContent>
+    <FieldSet>
+      <Button onClick={hideInformation} label={<T i18nKey="button_next" />} />
+    </FieldSet>
+  </Layout>
+);
 
 export default Information;
