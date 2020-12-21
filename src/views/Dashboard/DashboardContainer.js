46c2c3b9707e4bf663b322fa334927204cf7b6a7
TomaszMolenda <tomasz.molenda@thecoders.io>
2020-12-21 14:55:20 +0100

    PSAFE-3354 - add fetch Lab Test Subscription on dashboard (#692)

--- src/views/Dashboard/DashboardContainer.js
+++ src/views/Dashboard/DashboardContainer.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Dashboard from './Dashboard';
+import { fetchLabTestSubscription } from '../../store/actions/nativeData';
 
 const DashboardContainer = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchLabTestSubscription());
+    // eslint-disable-next-line
+  }, []);
   return <Dashboard />;
 };
 
