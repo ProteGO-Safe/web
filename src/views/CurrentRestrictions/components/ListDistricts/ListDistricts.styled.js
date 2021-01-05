89a788b34b00694fec31ecad1de55c8801defba5
Mateusz Andruszko <64523688+mateuszandruszko@users.noreply.github.com>
2021-01-05 09:53:37 +0100

    PSAFE-3396 (#706)

--- src/views/CurrentRestrictions/components/ListDistricts/ListDistricts.styled.js
+++ src/views/CurrentRestrictions/components/ListDistricts/ListDistricts.styled.js
@@ -2,4 +2,5 @@ import styled from 'styled-components';
 
 export const ListDistricts = styled.div`
   width: 100%;
+  margin-bottom: 22px;
 `;
--- src/views/Dashboard/components/ResultAnalysis/components/CustomCallToActionButton/CustomCallToActionButton.styled.js
+++ src/views/Dashboard/components/ResultAnalysis/components/CustomCallToActionButton/CustomCallToActionButton.styled.js
@@ -3,4 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   width: 100%;
   margin-top: 20px;
+  & + div {
+    margin-top: 20px;
+  }
 `;
