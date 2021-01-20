61109bd86855b431a3c3d3d1ce95d1393ea4a1aa
TomaszMolenda <tomasz.molenda@thecoders.io>
2021-01-20 18:51:24 +0100

    PSAFE-3116 - validate locales (#719)

---
+++ locales/scripts/format_checker.py
@@ -0,0 +1,30 @@
+import json
+import sys
+
+from validators import SingleTag, SpaceAtTheBeginOrEnd, ClosingTag, SplitFormat
+
+
+validators = [SpaceAtTheBeginOrEnd(), SingleTag(), ClosingTag(), SplitFormat()]
+
+lang = sys.argv[1]
+pl_file = open(f"locales/pl.json", )
+checking_file = open(f"locales/{lang}.json", )
+pl_json = json.load(pl_file)
+checking_json = json.load(checking_file)
+validate_results = []
+for pl_key in pl_json:
+    pl_value = pl_json[pl_key]
+    checking_value = checking_json[pl_key]
+    for validator in validators:
+        validate_result = validator.validate(pl_value, checking_value)
+        validate_results.append(validate_result)
+
+for validate_result in validate_results:
+    if validate_result.valid is False:
+        print(validate_result.message)
+        print("\n")
+
+pl_file.close()
+checking_file.close()
+if len(validate_results) != 0:
+    sys.exit(1)
---
+++ locales/scripts/validators.py
@@ -0,0 +1,85 @@
+import re
+
+
+def prepare_invalid_tags(tag):
+    return [
+        f"[ {tag}]",
+        f"[  {tag}]",
+        f"[{tag} ]",
+        f"[{tag}  ]",
+        f"[ {tag} ]",
+        f"[  {tag} ]",
+        f"[ {tag}  ]",
+        f"[  {tag}  ]",
+        f"[ /{tag}]",
+        f"[/ {tag}]",
+        f"[ / {tag}]",
+        f"[/{tag} ]",
+        f"[/ {tag} ]",
+        f"[ /{tag} ]",
+        f"[ / {tag} ]",
+    ]
+
+
+def prepare_valid_tag(tag):
+    return f"[{tag}]"
+
+
+def prepare_closing_tag(tag):
+    return f"[/{tag}]"
+
+
+class ValidateResult:
+    def __init__(self, valid, message=''):
+        self.valid = valid
+        self.message = message
+
+
+class SpaceAtTheBeginOrEnd:
+    @staticmethod
+    def validate(pl_value, checking_value):
+        if pl_value[0] is ' ' and checking_value[0] is not ' ':
+            return ValidateResult(False, f"missing space in ${checking_value}")
+        if pl_value[-1] is ' ' and checking_value[-1] is not ' ':
+            return ValidateResult(False, f"missing space in ${checking_value}")
+        return ValidateResult(True)
+
+
+class SingleTag:
+    @staticmethod
+    def validate(pl_value, checking_value):
+        tags = ["WBR", "COVID-19", "BR", "PP", "REG", "PHONE", "B", "EMAIL", "URL", "ROUTE", "COLOR"]
+        for tag in tags:
+            for invalid_tag in prepare_invalid_tags(tag):
+                if invalid_tag in checking_value:
+                    return ValidateResult(False, f"illegal tag in ${checking_value}")
+        return ValidateResult(True)
+
+
+class ClosingTag:
+    @staticmethod
+    def validate(pl_value, checking_value):
+        tags = ["PP", "REG", "PHONE", "B", "EMAIL", "URL", "ROUTE", "COLOR"]
+        for tag in tags:
+            open_occurrences = checking_value.count(prepare_valid_tag(tag))
+            close_occurrences = checking_value.count(prepare_closing_tag(tag))
+            if open_occurrences != close_occurrences:
+                return ValidateResult(False, f"missing  closing tag in ${checking_value}")
+        return ValidateResult(True)
+
+
+class SplitFormat:
+    @staticmethod
+    def validate(pl_value, checking_value):
+        tags = ["URL", "COLOR", "ROUTE"]
+        for tag in tags:
+            result = re.findall("\[" + tag + "\](.*?)\[/" + tag + "\]", checking_value)
+            for item in result:
+                split = item.split('|')
+                if len(split) != 2:
+                    return ValidateResult(False, f"illegal {tag} format in {checking_value}")
+                if split[0][0] is ' ' or split[0][-1] is ' ':
+                    return ValidateResult(False, f"illegal {tag} format in {checking_value}")
+                if split[1][0] is ' ' or split[1][-1] is ' ':
+                    return ValidateResult(False, f"illegal {tag} format in {checking_value}")
+        return ValidateResult(True)
