9dd329e68e46473343f4c9b747a386f8b9a549e6
Mateusz Andruszko <mateusz.andruszko@thecoders.io>
2021-01-26 13:12:12 +0100

    PSAFE-3420

--- src/views/HowItWorks/HowItWorks.styled.js
+++ src/views/HowItWorks/HowItWorks.styled.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 import { Color } from '../../theme/colors';
 
-import { ReactComponent as ImageSVG } from '../../assets/img/icons/how-it-works-image.svg';
+import { ReactComponent as ImageSVG } from '../../assets/img/icons/how-it-works.svg';
 import { ReactComponent as IconWarning } from '../../assets/img/icons/icon-no-data.svg';
 
 export const MainTitle = styled.h1`
   width: 100%;
-  margin: 0 0 20px 0;
+  margin: ${({ mgrBottom }) => (mgrBottom ? '0 0 20px 0' : '0')};
   font-size: 20px;
   line-height: 24px;
   font-weight: 700;
@@ -14,17 +14,18 @@ export const MainTitle = styled.h1`
 `;
 export const Image = styled(ImageSVG)`
   display: block;
-  max-height: 110px;
-  margin: 6px auto 20px;
+  max-height: 136px;
+  margin: 0 auto 24px;
 `;
 export const Paragraph = styled.p`
   width: 100%;
-  margin-bottom: 20px;
+  margin-bottom: 24px;
   font-size: 14px;
   line-height: 20px;
   font-weight: 400;
   color: ${Color.black};
-  strong {
+  strong,
+  a {
     font-weight: 700;
   }
 `;
@@ -33,38 +34,36 @@ export const Title = styled.h2`
   margin: ${({ mgrTop }) => (mgrTop ? '20px 0 16px 0' : '0 0 16px 0')};
   font-size: 16px;
   line-height: 22px;
-  font-weight: 600;
+  font-weight: ${({ semiBold }) => (semiBold ? '600' : '700')};
   color: ${Color.black};
 `;
 export const Warning = styled.div`
   display: flex;
   flex-flow: wrap row;
   width: 100%;
-  margin-bottom: 20px;
+  margin-bottom: 24px;
   padding: 14px 16px;
   border-radius: 4px;
   border: 1px solid ${Color.primary};
   color: ${Color.primary};
 `;
 export const Icon = styled(IconWarning)`
-  width: 24px;
-  height: 24px;
+  width: 28px;
+  height: 28px;
   margin-right: 16px;
 `;
 export const Description = styled.div`
-  width: calc(100% - 48px);
+  width: calc(100% - 44px);
   font-size: 12px;
-  line-height: 20px;
+  line-height: 18px;
   font-weight: 400;
   color: ${Color.black};
-`;
-export const Link = styled.a`
-  display: inline-block;
-  font-weight: 700;
-  color: ${Color.primary};
+  > strong {
+    font-weight: 700;
+  }
 `;
 export const ListNumber = styled.ol`
-  margin-top: 0;
+  margin: 0 0 24px 0;
   padding-left: 0;
   list-style: none;
   counter-reset: counter;
