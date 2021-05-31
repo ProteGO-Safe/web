4f8ce95034f3bb09bab2763337240ca5a77e23eb
Mateusz Andruszko <64523688+mateuszandruszko@users.noreply.github.com>
2021-05-31 15:08:21 +0200

    PSAFE-3721 - Styled view (#815)

--- src/views/PinReceiver/PinReceiver.js
+++ src/views/PinReceiver/PinReceiver.js
@@ -1,29 +1,36 @@
 import React from 'react';
-import * as Styled from './PinReceiver.styled';
 import { Button, Layout, T } from '../../components';
+import * as Styled from './PinReceiver.styled';
 
-const PinReceiver = ({ onClickButton }) => {
-  // todo https://kyotu.atlassian.net/browse/PSAFE-3721
-  return (
-    <Layout isNavigation>
-      <Styled.Title>
-        <T i18nKey="pin_receiver_text_01" />
-      </Styled.Title>
-      <Styled.Text>
-        <T i18nKey="pin_receiver_text_02" />
-      </Styled.Text>
+const PinReceiver = ({ onClickButton }) => (
+  <Layout isNavigation>
+    <Styled.Title>
+      <T i18nKey="pin_receiver_text_01" />
+    </Styled.Title>
+
+    <Styled.Text>
+      <T i18nKey="pin_receiver_text_02" />
+    </Styled.Text>
+
+    <Styled.List>
       <Styled.Item>
         <T i18nKey="pin_receiver_text_03" />
       </Styled.Item>
+    </Styled.List>
+
+    <Styled.ButtonWrapper>
       <Button onClick={onClickButton} label={<T i18nKey="pin_receiver_text_04" />} />
+    </Styled.ButtonWrapper>
+
+    <Styled.List>
       <Styled.Item>
         <T i18nKey="pin_receiver_text_05" />
       </Styled.Item>
       <Styled.Item>
         <T i18nKey="pin_receiver_text_06" />
       </Styled.Item>
-    </Layout>
-  );
-};
+    </Styled.List>
+  </Layout>
+);
 
 export default PinReceiver;
